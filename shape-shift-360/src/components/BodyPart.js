2ca40aca67e8b7c2b159c41ea1fff750785e1648
Shobhit Aswal <116807326+shobhitaswal@users.noreply.github.com>
2023-08-19 17:52:23 +0530

    ongoing project

---
+++ shape-shift-360/src/components/BodyPart.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
+
+const BodyPart = () => {
+  return (
+    <Stack>
+      <img
+        src={Icon}
+        alt="dumbbell"
+        style={{ width: "40px", height: "40px" }}
+      />
+    </Stack>
+  );
+};
+
+export default BodyPart;
